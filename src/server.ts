92c764b8a24d5b4d82990f552e31478c188687ff
joschonarth <joschonarth@gmail.com>
2025-02-06 15:19:29 -0300

    chore: setup eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 const port = env.PORT
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
