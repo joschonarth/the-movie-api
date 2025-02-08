936816ea6e47af7a7def5b6246567b1dff5651c2
joschonarth <joschonarth@gmail.com>
2025-02-08 14:23:50 -0300

    feat: implemente custon unauthorized error

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,40 +1,31 @@
 import { env } from '@/env'
-import {
-  FastifyInstance,
-  FastifyPluginAsync,
-  FastifyRequest,
-  FastifyReply,
-} from 'fastify'
+import { UnauthorizedError } from '@/errors/unauthorized-error'
+import { FastifyInstance, FastifyPluginAsync, FastifyRequest } from 'fastify'
 import fp from 'fastify-plugin'
 
 export const authMiddleware: FastifyPluginAsync = fp(
   async (fastify: FastifyInstance) => {
-    fastify.addHook(
-      'onRequest',
-      async (request: FastifyRequest, reply: FastifyReply) => {
-        const { authorization } = request.headers
+    fastify.addHook('onRequest', async (request: FastifyRequest) => {
+      const { authorization } = request.headers
 
-        if (!authorization || !authorization.startsWith('Basic ')) {
-          reply.code(401).send({ error: 'Unauthorized' })
-          return
-        }
+      if (!authorization || !authorization.startsWith('Basic ')) {
+        throw new UnauthorizedError()
+      }
 
-        const base64Credentials = authorization.split(' ')[1]
-        const credentials = Buffer.from(base64Credentials, 'base64').toString(
-          'utf-8',
-        )
-        const [username, password] = credentials.split(':')
+      const base64Credentials = authorization.split(' ')[1]
+      const credentials = Buffer.from(base64Credentials, 'base64').toString(
+        'utf-8',
+      )
+      const [username, password] = credentials.split(':')
 
-        const ADMIN_USER = env.ADMIN_USER
-        const ADMIN_PASSWORD = env.ADMIN_PASSWORD
+      const ADMIN_USER = env.ADMIN_USER
+      const ADMIN_PASSWORD = env.ADMIN_PASSWORD
 
-        if (username !== ADMIN_USER || password !== ADMIN_PASSWORD) {
-          reply.code(401).send({ error: 'Unauthorized' })
-          return
-        }
+      if (username !== ADMIN_USER || password !== ADMIN_PASSWORD) {
+        throw new UnauthorizedError()
+      }
 
-        request.user = { username }
-      },
-    )
+      request.user = { username }
+    })
   },
 )
